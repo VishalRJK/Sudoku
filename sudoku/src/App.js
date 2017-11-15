437a71a8c8a7d101ce018e5f21dee87ea4ead346
JK <621737@cognizant.com>
2017-11-15 14:16:11 +0530

    added sudoku app

--- sudoku/src/App.js
+++ sudoku/src/App.js
@@ -1,18 +1,61 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import './App.css'
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state={numbers:{'r1c1':'','r1c2':'','r1c3':'','r2c1':'','r2c2':'','r2c3':'','r3c1':'','r3c2':'','r3c3':''}};
+    this.startGame=this.startGame.bind(this);
+  }  
+  startGame() {
+    debugger;
+    var keys = Object.keys(this.state.numbers);
+    var random1= keys[Math.floor(Math.random()*8)];
+    var random2= keys[Math.floor(Math.random()*8)];
+    var random3= keys[Math.floor(Math.random()*8)];
+    if(random1 !== random2 && random1 !== random3 && random2 !== random3)
+    {
+    this.setState(prevState => {return{
+    numbers: {
+        ...prevState.numbers,
+       random1: Math.floor((1+Math.random()*9)),
+       random2: Math.floor((1+Math.random()*9)),
+       random3: Math.floor((1+Math.random()*9)),
+             }
+}});
+    }
+    else
+    {    
+    this.startGame();
+    }
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Sudoku</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="App-intro">
+          <table >
+            <tbody>
+            <tr>
+              <td>{this.state.numbers.r1c1}</td>
+              <td>{this.state.numbers.r1c2}</td>
+              <td>{this.state.numbers.r1c3}</td>
+            </tr>
+            <tr>
+              <td>{this.state.numbers.r2c1}</td>
+              <td>{this.state.numbers.r2c2}</td>
+              <td>{this.state.numbers.r2c3}</td>
+            </tr>
+            <tr>
+              <td>{this.state.numbers.r3c1}</td>
+              <td>{this.state.numbers.r3c2}</td>
+              <td>{this.state.numbers.r3c3}</td>
+            </tr>
+            </tbody>
+          </table>
+          <button onClick ={this.startGame}>Start The Game</button>
+        </div>
       </div>
     );
   }
